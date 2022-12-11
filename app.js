e2ab3c87981964fee5a6c7dd4a3cd67b367c8533
adimican2 <adimican@gmail.com>
2022-12-11 10:39:47 +0200

    use express get/post and parse input

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded());
+
 app.get('/currenttime', function(req, res) {
     res.send('<h1>' + new Date().toISOString() +'</h1>');
 });
 
 app.get('/', function(req, res) {
-    res.send('<h1>Hello world !!</h1>');
-    // res.send('<h1>Hello Word !</h1>');
+    res.send('<form action="/store-user"  method="POST"><label>Your Name</label><input type="text" name="username"><button>Submit</button></form>');
 });
+
+app.post('/store-user', function(req, res) {
+   const userName = req.body.username;
+   console.log(userName);
+   res.send('Name was stored !')
+})
 
 app.listen(3000);
 
